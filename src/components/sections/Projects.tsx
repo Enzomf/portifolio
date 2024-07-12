08596007118a0cd40d307256193dc225aeb45aa9
enzomf <enzomfigueiredo11@gmail.com>
2024-07-12 15:40:53 -0300

    altera o titulo da section

--- src/components/sections/Projects.tsx
+++ src/components/sections/Projects.tsx
@@ -9,7 +9,7 @@ export function Projects() {
 
   return (
     <section id="projects" className="col-span-full">
-      <h2 className="md:text-5xl text-3xl my-8 text-white font-bold">Projects</h2>
+      <h2 className="md:text-5xl text-3xl my-8 text-white font-bold">Projetos</h2>
       <div className="flex flex-col gap-6">
         {
           projects.map(({ description, link, name, stack, thumbnail }) => (
